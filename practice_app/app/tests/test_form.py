ba2d3fcecc6b293cd0e2ee7cbcf55e541f83b164
Vallley <86933389+Vallley@users.noreply.github.com>
2023-07-04 02:41:51 +0300

    feat: update project

--- practice_app/app/tests/test_form.py
+++ practice_app/app/tests/test_form.py
@@ -1,22 +1,34 @@
-from django.test import TestCase
-from app.forms import UserLoginForm, UserRegisterForm, FirmForm, PlaceForm, ProductionForm, PersonForm
-from app.models import Place, Production, Firm
 from django.contrib.auth.models import User
+from django.test import TestCase
+
+from app.forms import (
+    FirmForm,
+    PersonForm,
+    PlaceForm,
+    ProductionForm,
+    UserLoginForm,
+    UserRegisterForm,
+)
+from app.models import Firm, Place, Production
+
 
 class TestForms(TestCase):
-
     def setUp(self):
-        self.place1 = Place.objects.create(place = 'place 1')
-        self.prod1 = Production.objects.create(title = 'prod 1')
-        self.firm1 = Firm.objects.create(title = 'fk_firm', slug = 'fk_firm', main_inf = 'some inf', photo = 'photo', location = 'location')
+        self.place1 = Place.objects.create(place="place 1")
+        self.prod1 = Production.objects.create(title="prod 1")
+        self.firm1 = Firm.objects.create(
+            title="fk_firm",
+            slug="fk_firm",
+            main_inf="some inf",
+            photo="photo",
+            location="location",
+        )
         self.user = User.objects.create_user(
-            username='test', email='mail@gmail.com', password='testpassword'
+            username="test", email="mail@gmail.com", password="testpassword"
         )
 
     def test_place_form_valid(self):
-        form = PlaceForm(data={
-            'place' : 'place1'
-        })
+        form = PlaceForm(data={"place": "place1"})
 
         self.assertTrue(form.is_valid())
 
@@ -26,90 +38,90 @@ class TestForms(TestCase):
         self.assertEquals(len(form.errors), 1)
 
     def test_production_form_valid(self):
-        form = ProductionForm(data={
-            'title' : 'prod1'
-        })
+        form = ProductionForm(data={"title": "prod1"})
 
         self.assertTrue(form.is_valid())
 
-    def test_place_form_invalid(self):
+    def test_productions_form_invalid(self):
         form = ProductionForm(data={})
         self.assertFalse(form.is_valid())
-        self.assertEquals(len(form.errors), 1)    
+        self.assertEquals(len(form.errors), 1)
 
     def test_firm_form_valid(self):
-        form = FirmForm(data={
-            "title" : 'firm1',
-            "main_inf" : 'some inf',
-            "location" : 'location',
-            "place" : [self.place1],
-            "productions" : [self.prod1],
-        })
+        form = FirmForm(
+            data={
+                "title": "firm1",
+                "main_inf": "some inf",
+                "location": "location",
+                "place": [self.place1],
+                "productions": [self.prod1],
+            }
+        )
         self.assertTrue(form.is_valid())
 
     def test_firm_form_invalid(self):
-        form = FirmForm(data={'title': 'title'})
+        form = FirmForm(data={"title": "title"})
         self.assertFalse(form.is_valid())
-        self.assertEquals(len(form.errors), 2)  
+        self.assertEquals(len(form.errors), 2)
 
     def test_person_form_valid(self):
-        form = PersonForm(data={
-            "name" : 'person1',
-            "main_inf" : 'some inf',
-            "age" : 10,
-            "gender" : 'm',
-            "adress" : 'adress',
-            "nationality" : 'nat',
-            "family_status" : 'status',
-            "profession" : 'prof',
-            "education" : 'edu',
-            "experience" : 'exp',
-            "current_place" : self.firm1,
-            "telegram" : 'telegram',
-            "viber" : 'viber',
-            "skype" : 'skype',
-            "mail" : 'mail@gmail.com',
-            "phone" : 'phone',
-        })
+        form = PersonForm(
+            data={
+                "name": "person1",
+                "main_inf": "some inf",
+                "age": 10,
+                "gender": "m",
+                "adress": "adress",
+                "nationality": "nat",
+                "family_status": "status",
+                "profession": "prof",
+                "education": "edu",
+                "experience": "exp",
+                "current_place": self.firm1,
+                "telegram": "telegram",
+                "viber": "viber",
+                "skype": "skype",
+                "mail": "mail@gmail.com",
+                "phone": "phone",
+            }
+        )
         self.assertTrue(form.is_valid())
 
     def test_person_form_invalid(self):
-        form = PersonForm(data={'name': 'name'})
+        form = PersonForm(data={"name": "name"})
         self.assertFalse(form.is_valid())
-        self.assertEquals(len(form.errors), 12)    
+        self.assertEquals(len(form.errors), 12)
 
     def test_register_form_valid(self):
         form_data = {
-            'username' : 'testuser',
-            'email' : 'email@gmail.com',
-            'password1' : 'testpassword',
-            'password2' : 'testpassword',
+            "username": "testuser",
+            "email": "email@gmail.com",
+            "password1": "testpassword",
+            "password2": "testpassword",
         }
         form = UserRegisterForm(data=form_data)
         self.assertTrue(form.is_valid())
-        self.assertEqual(form.fields['username'].label, 'Имя пользователя')
-        self.assertEqual(form.fields['email'].label, 'Email')
-        self.assertEqual(form.fields['password1'].label, 'Пароль')
-        self.assertEqual(form.fields['password2'].label, 'Подтверждение пароля')
+        self.assertEqual(form.fields["username"].label, "Имя пользователя")
+        self.assertEqual(form.fields["email"].label, "Email")
+        self.assertEqual(form.fields["password1"].label, "Пароль")
+        self.assertEqual(form.fields["password2"].label, "Подтверждение пароля")
 
     def test_register_form_invalid(self):
         form = UserRegisterForm(data={})
         self.assertFalse(form.is_valid())
-        self.assertEquals(len(form.errors), 4)  
+        self.assertEquals(len(form.errors), 4)
 
     def test_login_form_valid(self):
         form_data = {
-            'username' : 'test',
-            'password' : 'testpassword',
+            "username": "test",
+            "password": "testpassword",
         }
         form = UserLoginForm(data=form_data)
         self.assertTrue(form.is_valid())
-        self.assertEqual(form.fields['username'].label, 'Имя пользователя')
-        self.assertEqual(form.fields['password'].label, 'Пароль')
+        self.assertEqual(form.fields["username"].label, "Имя пользователя")
+        self.assertEqual(form.fields["password"].label, "Пароль")
 
     def test_login_form_invalid(self):
         form = UserLoginForm(data={})
         self.assertFalse(form.is_valid())
-        self.assertEquals(len(form.errors), 2)  
-
-    
+        self.assertEquals(len(form.errors), 2)
--- practice_app/app/tests/test_urls.py
+++ practice_app/app/tests/test_urls.py
@@ -1,53 +1,67 @@
 from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-from app.views import FirmView, CreateFirm, register, user_login, user_logout, CreatePerson, CreateProduction, CreatePlace, FilterFirmsView, PersonView, FirmPage, PersonPage 
- 
+from django.urls import resolve, reverse
+
+from app.views import (
+    CreateFirm,
+    CreatePerson,
+    CreatePlace,
+    CreateProduction,
+    FilterFirmsView,
+    FirmPage,
+    FirmView,
+    PersonPage,
+    PersonView,
+    register,
+    user_login,
+    user_logout,
+)
+
+
 class TestUrls(SimpleTestCase):
-    
     def test_firms_page_url_is_resolved(self):
-        url = reverse('firms_page')
+        url = reverse("firms_page")
         self.assertEqual(resolve(url).func.view_class, FirmView)
 
     def test_filter_url_is_resolved(self):
-        url = reverse('filter')
-        self.assertEqual(resolve(url).func.view_class, FilterFirmsView)    
+        url = reverse("filter")
+        self.assertEqual(resolve(url).func.view_class, FilterFirmsView)
 
     def test_people_page_url_is_resolved(self):
-        url = reverse('people_page')
-        self.assertEqual(resolve(url).func.view_class, PersonView) 
+        url = reverse("people_page")
+        self.assertEqual(resolve(url).func.view_class, PersonView)
 
     def test_firm_url_is_resolved(self):
-        url = reverse('firm', args=['some-slug'])
+        url = reverse("firm", args=["some-slug"])
         self.assertEqual(resolve(url).func.view_class, FirmPage)
 
     def test_person_url_is_resolved(self):
-        url = reverse('person', args=['some-slug'])
+        url = reverse("person", args=["some-slug"])
         self.assertEqual(resolve(url).func.view_class, PersonPage)
 
     def test_add_firm_url_is_resolved(self):
-        url = reverse('add_firm')
-        self.assertEqual(resolve(url).func.view_class, CreateFirm)    
+        url = reverse("add_firm")
+        self.assertEqual(resolve(url).func.view_class, CreateFirm)
 
     def test_add_place_url_is_resolved(self):
-        url = reverse('add_place')
-        self.assertEqual(resolve(url).func.view_class, CreatePlace)          
+        url = reverse("add_place")
+        self.assertEqual(resolve(url).func.view_class, CreatePlace)
 
     def test_add_production_url_is_resolved(self):
-        url = reverse('add_production')
-        self.assertEqual(resolve(url).func.view_class, CreateProduction) 
+        url = reverse("add_production")
+        self.assertEqual(resolve(url).func.view_class, CreateProduction)
 
     def test_add_person_url_is_resolved(self):
-        url = reverse('add_person')
+        url = reverse("add_person")
         self.assertEqual(resolve(url).func.view_class, CreatePerson)
 
     def test_register_url_is_resolved(self):
-        url = reverse('register')
+        url = reverse("register")
         self.assertEqual(resolve(url).func, register)
 
     def test_login_url_is_resolved(self):
-        url = reverse('login')
+        url = reverse("login")
         self.assertEqual(resolve(url).func, user_login)
 
     def test_logout_url_is_resolved(self):
-        url = reverse('logout')
-        self.assertEqual(resolve(url).func, user_logout)     
+        url = reverse("logout")
+        self.assertEqual(resolve(url).func, user_logout)
